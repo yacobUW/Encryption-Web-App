0ab37ca473c252e7d219e72a4f890189f8d2c750
yacobUW <122832467+yacobUW@users.noreply.github.com>
2023-08-06 13:46:07 -0700

    Update APP.js

--- js/react/APP.js
+++ js/react/APP.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import crypto from 'crypto';
 import aesEncrypt from '../encryption/AES_encryption'; // Replace with the correct path to AES_encryption.js
 import aesDecrypt from '../decryption/AES_decryption'; // Replace with the correct path to AES_decryption.js
 import './styles.css';
