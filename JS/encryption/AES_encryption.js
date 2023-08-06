76cf550a0a3406ebee556456f5cf6e559ca1331f
yacobUW <122832467+yacobUW@users.noreply.github.com>
2023-08-05 23:37:15 -0700

    Update AES_encryption.js

--- JS/encryption/AES_encryption.js
+++ JS/encryption/AES_encryption.js
@@ -1,1 +1,15 @@
+const CryptoJS = require('crypto-js');
+
+// AES Encryption function
+function aesEncrypt(plaintext, secretKey) {
+  try {
+    const ciphertext = CryptoJS.AES.encrypt(plaintext, secretKey).toString();
+    return ciphertext;
+  } catch (error) {
+    console.error('Error during AES encryption:', error.message);
+    return null;
+  }
+}
+
+module.exports = aesEncrypt;
 
