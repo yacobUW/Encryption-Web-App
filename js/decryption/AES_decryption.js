1c90bdd1d20d6ee09ce3cdab08499e6845bca8f1
yacobUW <122832467+yacobUW@users.noreply.github.com>
2023-08-06 13:44:12 -0700

    Update AES_decryption.js

--- js/decryption/AES_decryption.js
+++ js/decryption/AES_decryption.js
@@ -1,18 +1,30 @@
-// const CryptoJS = require('crypto-js');
-// AES_decryption.js
-import CryptoJS from 'crypto-js';
-
 // AES Decryption function
-function aesDecrypt(ciphertext, secretKey) {
+async function aesDecrypt(ciphertext, secretKey) {
   try {
-    const bytes = CryptoJS.AES.decrypt(ciphertext, secretKey);
-    const plaintext = bytes.toString(CryptoJS.enc.Utf8);
-    return plaintext;
+    const binaryData = atob(ciphertext);
+    const iv = binaryData.slice(0, 16);
+    const ciphertextData = binaryData.slice(16);
+
+    const key = await window.crypto.subtle.importKey(
+      'raw',
+      new TextEncoder().encode(secretKey),
+      { name: 'AES-CBC', length: 256 },
+      false,
+      ['decrypt']
+    );
+
+    const decryptedData = await window.crypto.subtle.decrypt(
+      { name: 'AES-CBC', iv: new Uint8Array(iv) },
+      key,
+      new Uint8Array(ciphertextData)
+    );
+
+    const decryptedText = new TextDecoder().decode(decryptedData);
+    return decryptedText;
   } catch (error) {
     console.error('Error during AES decryption:', error.message);
     return null;
   }
 }
 
-module.exports = aesDecrypt;
-
+export default aesDecrypt;
