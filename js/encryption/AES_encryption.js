aab248c55292d95ba159d6a0fb30bd649808769d
yacobUW <122832467+yacobUW@users.noreply.github.com>
2023-08-06 13:44:35 -0700

    Update AES_encryption.js

--- js/encryption/AES_encryption.js
+++ js/encryption/AES_encryption.js
@@ -1,17 +1,29 @@
-//const CryptoJS = require('crypto-js');
-// AES_encryption.js
-import CryptoJS from 'crypto-js';
-
 // AES Encryption function
-function aesEncrypt(plaintext, secretKey) {
+async function aesEncrypt(plaintext, secretKey) {
   try {
-    const ciphertext = CryptoJS.AES.encrypt(plaintext, secretKey).toString();
-    return ciphertext;
+    const encodedText = new TextEncoder().encode(plaintext);
+    const key = await window.crypto.subtle.importKey(
+      'raw',
+      new TextEncoder().encode(secretKey),
+      { name: 'AES-CBC', length: 256 },
+      false,
+      ['encrypt']
+    );
+
+    const iv = window.crypto.getRandomValues(new Uint8Array(16));
+
+    const ciphertext = await window.crypto.subtle.encrypt(
+      { name: 'AES-CBC', iv: iv },
+      key,
+      encodedText
+    );
+
+    const encryptedText = btoa(iv + new Uint8Array(ciphertext));
+    return encryptedText;
   } catch (error) {
     console.error('Error during AES encryption:', error.message);
     return null;
   }
 }
 
-module.exports = aesEncrypt;
-
+export default aesEncrypt;
