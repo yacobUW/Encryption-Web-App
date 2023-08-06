8c2ebf91e458e9991873a8dfa9fbb7caa60c691d
yacobUW <122832467+yacobUW@users.noreply.github.com>
2023-08-05 23:36:48 -0700

    Update AES_decryption.js

--- JS/decryption/AES_decryption.js
+++ JS/decryption/AES_decryption.js
@@ -1,1 +1,16 @@
+const CryptoJS = require('crypto-js');
+
+// AES Decryption function
+function aesDecrypt(ciphertext, secretKey) {
+  try {
+    const bytes = CryptoJS.AES.decrypt(ciphertext, secretKey);
+    const plaintext = bytes.toString(CryptoJS.enc.Utf8);
+    return plaintext;
+  } catch (error) {
+    console.error('Error during AES decryption:', error.message);
+    return null;
+  }
+}
+
+module.exports = aesDecrypt;
 
