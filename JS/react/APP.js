c0399a65b8be78f9c1e86f18395f759d73ed88c6
yacobUW <122832467+yacobUW@users.noreply.github.com>
2023-08-05 23:32:55 -0700

    Update APP.js

--- JS/react/APP.js
+++ JS/react/APP.js
@@ -1,74 +1,70 @@
 import React, { useState } from 'react';
+import './styles.css';
 
 function App() {
-  // State variables to hold input and output values
   const [inputText, setInputText] = useState('');
   const [encryptedText, setEncryptedText] = useState('');
   const [decryptedText, setDecryptedText] = useState('');
 
-  // Function to handle encryption
   const handleEncryption = () => {
-    // Your AES encryption logic goes here
+    // Your AES encryption logic here
     // Use the 'inputText' state variable for the input text
     // Set the result in the 'encryptedText' state variable
   };
 
-  // Function to handle decryption
   const handleDecryption = () => {
-    // Your AES decryption logic goes here
+    // Your AES decryption logic here
     // Use the 'encryptedText' state variable for the input text
     // Set the result in the 'decryptedText' state variable
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>AES Encryption Web App</h1>
 
       {/* Encryption Form */}
-      <form onSubmit={handleEncryption}>
+      <div className="form-group">
         <label htmlFor="inputText">Enter text to encrypt:</label>
         <textarea
           id="inputText"
           value={inputText}
           onChange={(e) => setInputText(e.target.value)}
           rows="4"
-          cols="50"
         ></textarea>
-        <br />
-        <button type="submit">Encrypt</button>
-      </form>
-      <div>
-        {/* Display Encrypted Text */}
-        {encryptedText && (
-          <div>
-            <strong>Encrypted Text:</strong>
-            <div>{encryptedText}</div>
-          </div>
-        )}
+      </div>
+      <div className="form-group">
+        <button onClick={handleEncryption}>Encrypt</button>
       </div>
 
+      {/* Display Encrypted Text */}
+      {encryptedText && (
+        <div className="result">
+          <strong>Encrypted Text:</strong>
+          <div>{encryptedText}</div>
+        </div>
+      )}
+
       {/* Decryption Form */}
-      <form onSubmit={handleDecryption}>
+      <div className="form-group">
         <label htmlFor="encryptedText">Enter encrypted text to decrypt:</label>
         <textarea
           id="encryptedText"
           value={encryptedText}
           onChange={(e) => setEncryptedText(e.target.value)}
           rows="4"
-          cols="50"
         ></textarea>
-        <br />
-        <button type="submit">Decrypt</button>
-      </form>
-      <div>
-        {/* Display Decrypted Text */}
-        {decryptedText && (
-          <div>
-            <strong>Decrypted Text:</strong>
-            <div>{decryptedText}</div>
-          </div>
-        )}
       </div>
+      <div className="form-group">
+        <button onClick={handleDecryption}>Decrypt</button>
+      </div>
+
+      {/* Display Decrypted Text */}
+      {decryptedText && (
+        <div className="result">
+          <strong>Decrypted Text:</strong>
+          <div>{decryptedText}</div>
+        </div>
+      )}
     </div>
   );
 }
