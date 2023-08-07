f6707be7d916c0dbedb236feda99373af0aaa2e9
yacobUW <122832467+yacobUW@users.noreply.github.com>
2023-08-07 13:57:45 -0700

    Update AES_encryption.js

--- js/encryption/AES_encryption.js
+++ js/encryption/AES_encryption.js
@@ -1,22 +1,11 @@
+const CryptoJS = require('crypto-js');
+
 // AES Encryption function
 async function aesEncrypt(plaintext, secretKey) {
   try {
-    const iv = window.crypto.getRandomValues(new Uint8Array(16));
-    const key = await window.crypto.subtle.importKey(
-      'raw',
-      new TextEncoder().encode(secretKey),
-      { name: 'AES-CBC', length: 256 },
-      false,
-      ['encrypt']
-    );
-
-    const encryptedData = await window.crypto.subtle.encrypt(
-      { name: 'AES-CBC', iv },
-      key,
-      new TextEncoder().encode(plaintext)
-    );
-
-    const ciphertext = base64Encoding(iv, encryptedData);
+    const iv = CryptoJS.lib.WordArray.random(16);
+    const encrypted = CryptoJS.AES.encrypt(plaintext, secretKey, { iv });
+    const ciphertext = iv.concat(encrypted.ciphertext).toString(CryptoJS.enc.Base64);
     return ciphertext;
   } catch (error) {
     console.error('Error during AES encryption:', error.message);
@@ -24,10 +13,4 @@ async function aesEncrypt(plaintext, secretKey) {
   }
 }
 
-function base64Encoding(iv, data) {
-  const ivBase64 = btoa(String.fromCharCode.apply(null, iv));
-  const dataBase64 = btoa(String.fromCharCode.apply(null, new Uint8Array(data)));
-  return ivBase64 + dataBase64;
-}
-
-export default aesEncrypt;
+module.exports = aesEncrypt;
